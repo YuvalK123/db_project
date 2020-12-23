603547c92c5fc8ac908988f67d0fb16d1397c6c5
YuvalK <56754496+YuvalK123@users.noreply.github.com>
2020-12-23 10:07:59 +0200

    update initial arbitrary record
    update game table loading code to add users and games records

--- datasets/games/GameTables.py
+++ datasets/games/GameTables.py
@@ -23,10 +23,11 @@ def create_game_table():
 
 
 def letter_table():
-    #gid, char
+    # gid, char
     query = "CREATE TABLE game_letter (gid MEDIUMINT NOT NULL, letter CHAR(1)); "
     cursor.execute(query)
     db.commit()
+
 
 def locations_table():
     # gid, location
@@ -35,9 +36,8 @@ def locations_table():
     db.commit()
 
 
-
 def users():
-    #uid, username, password, age, gender
+    # uid, username, password, age, gender
     query = f"CREATE TABLE users (id MEDIUMINT PRIMARY KEY NOT NULL AUTO_INCREMENT, username VARCHAR(20)," \
             f"password VARCHAR(25), age TINYINT, gender CHAR(1)); "
     cursor.execute(query)
@@ -49,20 +49,40 @@ def users():
 
 def make_users():
     username, password, age, gender = "user", 12345, 26, "m"
-    query = f"INSERT INTO users (username, password, age, gender) VALUES('{username}', {password}, {age}, '{gender}')"
-    print(query)
+    query = f"INSERT INTO users (username, password, age, gender) " \
+            f"VALUES('{username}', {password}, {age}, '{gender}')"
     cursor.execute(query)
     adminname, password, age, gender = "admin", 123456, 25, "f"
-    query = f"INSERT INTO admins (username, password, age, gender) VALUES('{adminname}', {password}, {age}, '{gender}')"
+    query = f"INSERT INTO admins (username, password, age, gender) " \
+            f"VALUES('{adminname}', {password}, {age}, '{gender}')"
     cursor.execute(query)
     db.commit()
+
+
+def create_games():
+    # games =  gid, uid, current_score, strikes
+    # locations = gid, location
+    # letters = gid, letter
+    games = [(1, 3, 122, 2), (2, 5, 7, 5), (3, 22, 14, 1)]
+    locations = [(3, 1400), (3, 1412), (3, 502), (1, 202), (2, 305)]
+    letters = [(1, 'b'), (1, 'f'), (1, 'z'), (2, 'e'), (2, 'm'), (3, 'a'), (3, 'i')]
+    query = "INSERT INTO games (id, uid, current_score, strikes) VALUES (%s, %s, %s, %s);"
+    cursor.executemany(query, games)
+    query = "INSERT INTO game_locations (gid, location) VALUES (%s, %s);"
+    cursor.executemany(query, locations)
+    query = "INSERT INTO game_letter (gid, letter) VALUES (%s, %s);"
+    cursor.executemany(query, letters)
+    db.commit()
+
+
 
 def main():
     # create_game_table()
     # letter_table()
     # locations_table()
     # users()
-    make_users()
+    # make_users()
+    create_games()
 
 
 if __name__ == '__main__':
