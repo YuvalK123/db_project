ad9652f802ac89c917964326758e6edf9beb04e2
MorAl2 <58975745+MorAl2@users.noreply.github.com>
2021-01-04 20:14:09 +0200

    Add files via upload

--- ClientSide/MovieByPerson.js
+++ ClientSide/MovieByPerson.js
@@ -4,20 +4,33 @@ var port = 3000;
 $(document).ready(()=>{
     var person = $.urlParam("person");
     $("#nameActor").html(person)
-    $.ajax({ 
-        type: 'GET', 
-        contentType: "application/json; charset=utf-8",
-        url: "http://"+url+":"+port+'/movies?person='+person, 
-        success: function (data) {
-            if(data && data != "null"){
-                $("#resbtn").show();
-                oldGame = data;
-            }
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
+    data = {"gender": "m", "actedIn": [], "directed": [{"The Congres": ["Foreign", "Drama", "Animation"]}, {"Made in Israel": ["History", "War", "Family", "Crime"]}, {"Saint Clar": ["Horror", "Western"]}, {"Waltz with Bashir": ["War", "Foreign", "Crime"]}]}
 
-        }  
-    });
+    acted = data.actedIn
+    directed = data.directed
+    gender = data.gender
+
+    for (var t=0;t<acted.length;t++){
+        $("#movies").append("<p style=\"font-size: 18px;\">&#9675; "+Object.keys(acted[t])+" - as Actor - </p>")
+    }
+    for (var t=0;t<directed.length;t++){
+        $("#movies").append("<p style=\"font-size: 18px;\">&#9675; <b>"+Object.keys(directed[t])+"</b> - as Director - "+directed[t][Object.keys(directed[t])].join(",")+"</p>")
+    }
+
+    // $.ajax({ 
+    //     type: 'GET', 
+    //     contentType: "application/json; charset=utf-8",
+    //     url: "http://"+url+":"+port+'/movies?person='+person, 
+    //     success: function (data) {
+    //         if(data && data != "null"){
+    //             $("#resbtn").show();
+    //             oldGame = data;
+    //         }
+    //     },
+    //     error: function(XMLHttpRequest, textStatus, errorThrown) {
+
+    //     }  
+    // });
 })
 
 
