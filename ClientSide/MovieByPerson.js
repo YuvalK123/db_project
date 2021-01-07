605e82ece546323929220b7c773d885875909a81
mor <mor@mor.mor>
2021-01-07 19:23:58 +0200

    fix

--- ClientSide/MovieByPerson.js
+++ ClientSide/MovieByPerson.js
@@ -11,12 +11,14 @@ $(document).ready(()=>{
          contentType: "application/json; charset=utf-8",
          url: "http://"+url+":"+port+'/movies?person='+person,
          success: function (data) {
+             
              if(data && data != "null"){
+                data = JSON.parse(data)
                 acted = data.actedIn
                 directed = data.directed
                 gender = data.gender
                 for (var t=0;t<acted.length;t++){
-                    $("#movies").append("<p style=\"font-size: 18px;\">&#9675; "+Object.keys(acted[t])+" - as Actor - </p>")
+                    $("#movies").append("<p style=\"font-size: 18px;\">&#9675; "+Object.keys(acted[t])+" - as Actor - "+acted[t][Object.keys(acted[t])].join(",")+"</p>")
                 }
                 for (var t=0;t<directed.length;t++){
                     $("#movies").append("<p style=\"font-size: 18px;\">&#9675; <b>"+Object.keys(directed[t])+"</b> - as Director - "+directed[t][Object.keys(directed[t])].join(",")+"</p>")
